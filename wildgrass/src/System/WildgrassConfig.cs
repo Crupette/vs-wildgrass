c0cd87a766ff1261e8c102066f6ebda04349992a
Jon Santmyer <jon@jonsantmyer.com>
2025-04-18 16:31:45 -0400

    1.2.2

--- wildgrass/src/System/WildgrassConfig.cs
+++ wildgrass/src/System/WildgrassConfig.cs
@@ -1,11 +1,15 @@
 using System;
+using Newtonsoft.Json;
 using Vintagestory.API.Common;
 
 namespace Wildgrass;
 
 public class ConfigData
 {
+    [JsonProperty]
     public float GenerateDensity = 1.0f;
+    [JsonProperty]
+    public float SeedMultiplier = 1.0f;
 }
 
 public class WildgrassConfig : ModSystem
@@ -27,5 +31,19 @@ public class WildgrassConfig : ModSystem
             Data = data;
         }
         api.StoreModConfig(Data, "wildgrass.json");
+    }
+
+    public override void AssetsFinalize(ICoreAPI api)
+    {
+        base.AssetsFinalize(api);
+        WildgrassCore wildgrass = WildgrassCore.GetInstance(api);
+        foreach(BlockWildgrass block in wildgrass.WildgrassBlocks) {
+            foreach(var drop in block.Drops) {
+                if(drop.Code.BeginsWith("wildgrass", "seeds")) {
+                    drop.Quantity.avg *= Data.SeedMultiplier;
+                    drop.Quantity.var *= Data.SeedMultiplier;
+                }
+            }
+        }
     }
 }
--- wildgrass/src/System/WildgrassCore.cs
+++ wildgrass/src/System/WildgrassCore.cs
@@ -3,13 +3,13 @@ using Vintagestory.API.Server;
 using Vintagestory.API.Config;
 using Vintagestory.API.Common;
 using HarmonyLib;
+using System.Collections.Generic;
+using Vintagestory.API.Util;
 
 namespace Wildgrass
 {
     public class WildgrassCore : ModSystem
     {
-        public static AssetLocation[] WildgrassBlockCodes;
-
         Harmony harmony;
         public ICoreAPI api;
         public static bool IsDev = false;
@@ -28,9 +28,22 @@ namespace Wildgrass
             api.RegisterItemClass("wildgrass.ItemGrassSeeds", typeof(ItemGrassSeeds));
         }
 
+        public BlockWildgrass[] WildgrassBlocks => ObjectCacheUtil.GetOrCreate(api, "Wildgrass.WildgrassBlocks",  () => {
+            List<BlockWildgrass> blocks = new();
+            foreach(var block in api.World.Blocks) {
+                if(block is BlockWildgrass) blocks.Add(block as BlockWildgrass);
+            }
+            return blocks.ToArray();
+        });
+
         public override void Dispose()
         {
             harmony?.UnpatchAll(Mod.Info.ModID);
+            ObjectCacheUtil.Delete(api, "Wildgrass.WildgrassBlocks");
+        }
+
+        public static WildgrassCore GetInstance(ICoreAPI api) {
+            return api.ModLoader.GetModSystem<WildgrassCore>();
         }
     }
 }
